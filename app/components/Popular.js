f2e6f63f828891f9596ee8303aac3c85ce479e47
Bob Habig <rfhab2@gmail.com>
2018-02-13 00:29:55 -0500

    committing working and non-working solutions for TM react help

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import api from '../utils/api';
 
-// Stateless Functional Component
+//Stateless Functional Component
 function SelectLanguage (props) {
   let languages = [ 'All', 'JavaScript', 'Ruby', 'Elixir', 'CSS', 'Python', 'Go', 'Rust', 'R' ];
-
   return (
     <ul className="languages">
       {
-        languages.map( l => {
+        languages.map( lang => {
           return (
             <li
-              key={l}
-              onClick={props.onSelect.bind(null, l)}
-              style={l === props.selectedLanguage ? { color: 'red' } : null }>
-              {l}
+              key={lang}
+              onClick={props.onSelect.bind(null, lang)}
+              style={lang === props.selectedLanguage ? { color: 'red' } : null }>
+              {lang}
             </li>
           )
         })
@@ -28,33 +28,89 @@ SelectLanguage.propTypes = {
   onSelect: PropTypes.func.isRequired,
 }
 
+// working method #1 below:
 
 export default class Popular extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      selectedLanguage: 'All'
-    };
-
-    //this.updateLanguage = this.updateLanguage.bind(this);
-  }
-
-
-  render() {
-
-    const updateLanguage = lang => {
-      this.setState({
-        selectedLanguage: lang
-      })
+      selectedLanguage: 'All',
+      repos: null,
     }
 
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState({
+      selectedLanguage: lang,
+      repos: null,
+    });
+
+    api.fetchPopularRepos(this.state.selectedLanguage)
+      .then(resp => {
+        this.setState({
+          repos: resp,
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
+  render() {
     return (
       <div>
         <h1>Popular:</h1>
         <p>Selected Language: {this.state.selectedLanguage}</p>
-        <SelectLanguage selectedLanguage={this.state.selectedLanguage} onSelect={updateLanguage} />
+        <SelectLanguage selectedLanguage={this.state.selectedLanguage} onSelect={this.updateLanguage} />
       </div>
     )
   }
 }
+
+// non-working method below:
+// *why is there a syntax error when trying to use arrow Fn syntax for updateLanguage?
+
+// export default class Popular extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       selectedLanguage: 'All',
+//       repos: null,
+//     }
+//
+//     this.updateLanguage = this.updateLanguage.bind(this);
+//   }
+//
+//   updateLanguage = lang => { ****** <- get syntax error when trying to use arrow Fn ********
+//     this.setState({
+//       selectedLanguage: lang,
+//       repos: null,
+//     });
+//
+//     api.fetchPopularRepos(this.state.selectedLanguage)
+//       .then(resp => {
+//         this.setState({
+//           repos: resp,
+//         });
+//       });
+//   }
+//
+//   componentDidMount() {
+//     this.updateLanguage(this.state.selectedLanguage);
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h1>Popular:</h1>
+//         <p>Selected Language: {this.state.selectedLanguage}</p>
+//         <SelectLanguage selectedLanguage={this.state.selectedLanguage} onSelect={this.updateLanguage} />
+//       </div>
+//     )
+//   }
+// }
