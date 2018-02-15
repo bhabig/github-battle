cd00c381d99116f3eef50a46fe5c504894982fe6
Bob Habig <rfhab2@gmail.com>
2018-02-14 19:52:17 -0500

    forms video code

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,11 +1,113 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Battle = () => {
-  return (
-    <div>
-      Battle!!
-    </div>
-  )
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  onChange(e) {
+    let value = e.target.value;
+
+    this.setState(() => {
+      return {
+        username: value
+      }
+    })
+  }
+
+  handleSubmit(event) {
+    e.preventDefault();
+
+    this.props.onSubmit(
+      this.props.id,
+      this.state.username
+    )
+  }
+
+  render() {
+    return (
+      <form className="colum" onSubmit={this.handleSubmit}>
+        <label htmlFor="username" className="header">
+          {this.props.label}
+        </label>
+        <input
+          type="text"
+          id="username"
+          autoComplete='off'
+          value={this.state.username}
+          onChange={this.onChange}
+        />
+        <button
+          className='button'
+          type='submit'
+          disabled={!this.state.username}
+        >
+          Submit
+        </button>
+      </form>
+    )
+  }
 }
 
-export default Battle;
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+}
+
+export default class Battle extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: '',
+      playerTwoImage: '',
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit (id, username) {
+    this.setState((prevState, props) => {
+      return {
+        [id + 'Name']: username,
+        [id + 'Image']: 'https://github.com/'+username+'.png?size=200',
+      }
+    })
+  }
+
+  render() {
+    let playerOneName = this.state.playerOneName;
+    let playerTwoName = this.state.playerTwoName;
+
+    return (
+      <div>
+        <div className='row'>
+          {!playerOneName &&
+            <PlayerInput
+              id='playerOne'
+              label='Player One'
+              onSubmit={this.handleSubmit}
+            />}
+
+          {!playerTwoName &&
+            <PlayerInput
+              id='playerTwo'
+              label='Player Two'
+              onSubmit={this.handleSubmit}
+            />}
+        </div>
+      </div>
+    )
+  }
+}
